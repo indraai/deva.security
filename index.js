f78d3aca313f31e1571464f0c602521cf559d1f0
Quinn Michaels <quinn@indra.ai>
2025-01-28 17:41:15 -0800

    security deva to 2025 standards.

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-// Copyright (c)2023 Quinn Michaels
+// Copyright (c)2025 Quinn Michaels
 // Security Deva
 // Security Deva Manages Security in deva.world.
-const package = require('./package.json');
+import Deva from '@indra.ai/deva';
+import pkg from './package.json' with {type:'json'};
+
+import data from './data.json' with {type:'json'};
+const {agent,vars} = data.DATA;
+
+// set the __dirname
+import {dirname} from 'node:path';
+import {fileURLToPath} from 'node:url';    
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const info = {
-  id: package.id,
-  name: package.name,
-  describe: package.description,
-  version: package.version,
-  url: package.homepage,
+  id: pkg.id,
+  name: pkg.name,
+  describe: pkg.description,
+  version: pkg.version,
+  url: pkg.homepage,
   dir: __dirname,
-  git: package.repository.url,
-  bugs: package.bugs.url,
-  author: package.author,
-  license: package.license,
-  copyright: package.copyright,
+  git: pkg.repository.url,
+  bugs: pkg.bugs.url,
+  author: pkg.author,
+  license: pkg.license,
+  copyright: pkg.copyright,
 };
 
-const {agent,vars} = require('./data.json').DATA;
 
-const Deva = require('@indra.ai/deva');
 const SECURITY = new Deva({
   info,
   agent,
@@ -47,4 +55,4 @@ const SECURITY = new Deva({
   },
   methods: {},
 });
-module.exports = SECURITY
+export default SECURITY
