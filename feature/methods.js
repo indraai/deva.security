f4a6982c7d7c17de1c47130af543c103f613ff99
Quinn Michaels <quinn@indra.ai>
2025-06-05 11:16:42 -0700

    update to include signature feature

--- feature/methods.js
+++ feature/methods.js
@@ -92,4 +92,38 @@ export default {
     const cipher = `cipher: ${data.encrypted}`;
     return Promise.resolve(cipher);
   },
+  
+  async signature(packet) {
+    this.context('signature');
+    this.action('method', 'signature');
+    const id = this.lib.uid();
+    const date = this.lib.formatDate(Date.now(), 'long', true);
+    const agent = this.agent();
+    const name = this.client().profile.name;
+    const sigstr = `${id}${name}${date}`;
+    
+    const data = {
+      id,
+      name,
+      md5: this.lib.hash(sigstr, 'md5'),
+      sha256: this.lib.hash(sigstr, 'sha256'),
+      sha512: this.lib.hash(sigstr, 'sha512'),
+      date,
+    }
+    const text = [
+      `::begin:signature:${data.id}`,
+      `id: ${id}`,
+      `name: ${data.name}`,
+      `date: ${date}`,				
+      data.md5,
+      data.sha256,
+      data.sha512,
+      `::end:signature:${data.id}`,
+    ].join('\n').trim();
+    return {
+      text,
+      html: false,
+      data,
+    }	  
+  },
 }
